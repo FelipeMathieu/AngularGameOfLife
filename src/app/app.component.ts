aa37479dd85564a860c08a789b5d3d8ffc0e9b76
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-06-27 20:29:28 -0300

    Lazy loading field component to improve performance
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import { GameCardComponent } from './components/game-card/game-card.component';
 import { GameInfoComponent } from './components/game-info/game-info.component';
-import { GameFieldComponent } from './components/game-field/game-field.component';
 import { GameControlsComponent } from './components/game-controls/game-controls.component';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzSpinComponent } from 'ng-zorro-antd/spin';
+import { NgComponentOutlet } from '@angular/common';
+import { GameFieldComponent } from './components/game-field/game-field.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     GameCardComponent,
     GameInfoComponent,
-    GameFieldComponent,
     GameControlsComponent,
     NzFlexModule,
+    NzSpinComponent,
+    NgComponentOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  protected FieldComponent?: Type<GameFieldComponent>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this._LoadField();
+  }
+
+  private async _LoadField() {
+    const { GameFieldComponent } = await import(
+      './components/game-field/game-field.component'
+    );
+
+    this.FieldComponent = GameFieldComponent;
+  }
+}
