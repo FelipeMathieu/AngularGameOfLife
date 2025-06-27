49f0337efb88b0bf4a642fb02e7cb71cd591ac6f
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-06-27 18:10:41 -0300

    Add fps control feature
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/app/components/game-info/game-info.component.ts
+++ src/app/components/game-info/game-info.component.ts
@@ -6,10 +6,28 @@ import { AsyncPipe } from '@angular/common';
 import { NzCardComponent } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { UISelectorsService } from '../../core/services/ui-services/ui-selectors.service';
+import { NzSliderComponent } from 'ng-zorro-antd/slider';
+import { FormsModule } from '@angular/forms';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { FPS } from '../../common/constants';
+import { UIReducersService } from '../../core/services/ui-services/ui-reducers.service';
+import { LetDirective } from '@ngrx/component';
 
 @Component({
   selector: 'app-game-info',
-  imports: [NzGridModule, NzStatisticComponent, AsyncPipe, NzCardComponent],
+  imports: [
+    NzGridModule,
+    NzStatisticComponent,
+    AsyncPipe,
+    NzCardComponent,
+    NzSliderComponent,
+    FormsModule,
+    NzFlexModule,
+    NzTypographyModule,
+    AsyncPipe,
+    LetDirective,
+  ],
   templateUrl: './game-info.component.html',
   styleUrl: './game-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,13 +36,21 @@ export class GameInfoComponent {
   protected readonly Population$!: Observable<number>;
   protected readonly MaxPopulation$!: Observable<number>;
   protected readonly Generations$!: Observable<number>;
+  protected readonly Fps$!: Observable<number>;
+  protected readonly MaxFps = FPS;
 
   constructor(
     private readonly _creatureSelectors: CreatureSelectorsService,
-    private readonly _uiSelectors: UISelectorsService
+    private readonly _uiSelectors: UISelectorsService,
+    private readonly _uiReducers: UIReducersService
   ) {
     this.Population$ = this._creatureSelectors.Population$;
     this.MaxPopulation$ = this._creatureSelectors.MaxPopulation$;
     this.Generations$ = this._uiSelectors.Generations$;
+    this.Fps$ = this._uiSelectors.Fps$;
+  }
+
+  public OnChange(value: number) {
+    this._uiReducers.Fps = value;
   }
 }
