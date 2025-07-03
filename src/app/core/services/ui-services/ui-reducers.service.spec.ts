49a652869c482fac516351a64016cee217d71d3c
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-07-03 12:57:17 -0300

    Add ui reducers tests
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/app/core/services/ui-services/ui-reducers.service.spec.ts
+++ src/app/core/services/ui-services/ui-reducers.service.spec.ts
@@ -1,16 +1,54 @@
-import { TestBed } from '@angular/core/testing';
-
+import { FPS } from '../../../common/constants';
+import { gameUIStore } from '../../stores/game-ui-store';
 import { UIReducersService } from './ui-reducers.service';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
 
 describe('UiReducersService', () => {
-  let service: UIReducersService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UIReducersService);
+  let spectator: SpectatorService<UIReducersService>;
+  const createService = createServiceFactory({
+    service: UIReducersService,
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  const updateRunningStateAndMakeSureItHasBeenUpdated = () => {
+    const running = true;
+
+    spectator.service.Running = running;
+
+    expect(gameUIStore.getValue().running).toBeTrue();
+  };
+
+  beforeEach(() => {
+    spectator = createService();
+    spectator.service.Reset();
+  });
+
+  it('should update the "Fps" state', () => {
+    const fps = 15;
+
+    spectator.service.Fps = fps;
+
+    expect(gameUIStore.getValue().fps).toBe(fps);
+  });
+
+  it('should update the "running" state', () => {
+    updateRunningStateAndMakeSureItHasBeenUpdated();
+  });
+
+  it('should update the "generations" state', () => {
+    spectator.service.NextGeneration();
+
+    expect(gameUIStore.getValue().generations).toBe(1);
+  });
+
+  it('should reset the store', () => {
+    updateRunningStateAndMakeSureItHasBeenUpdated();
+
+    spectator.service.Reset();
+
+    const { fps, generations, running } = gameUIStore.getValue();
+
+    expect(fps).toBe(FPS);
+    expect(generations).toBe(0);
+    expect(running).toBeFalse();
   });
 });
