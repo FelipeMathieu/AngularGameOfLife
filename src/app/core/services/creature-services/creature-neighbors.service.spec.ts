e84c60afc50c65260c4814cb645e5ae629bbcc39
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-07-04 15:54:14 -0300

    Add tests to creature neighbors service
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/app/core/services/creature-services/creature-neighbors.service.spec.ts
+++ src/app/core/services/creature-services/creature-neighbors.service.spec.ts
@@ -1,16 +1,109 @@
-import { TestBed } from '@angular/core/testing';
-
+import {
+  createServiceFactory,
+  SpectatorService,
+  SpyObject,
+} from '@ngneat/spectator';
 import { CreatureNeighborsService } from './creature-neighbors.service';
+import { CreatureSelectorsService } from './creature-selectors.service';
+import { ICreature, TId } from '../../stores/creatures-store';
+
+const X = 10;
+const Y = 10;
+
+const baseCreature: ICreature = {
+  alive: false,
+  dirty: false,
+  id: `${X},${Y}`,
+  x: X,
+  y: Y,
+};
+
+const creatureNeighbors: ICreature[] = [
+  {
+    alive: false,
+    dirty: false,
+    id: `${X - 1},${Y}`,
+    x: X - 1,
+    y: Y,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X + 1},${Y}`,
+    x: X + 1,
+    y: Y,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X},${Y - 1}`,
+    y: Y - 1,
+    x: X,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X},${Y + 1}`,
+    y: Y + 1,
+    x: X,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X + 1},${Y + 1}`,
+    x: X + 1,
+    y: Y + 1,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X - 1},${Y + 1}`,
+    x: X - 1,
+    y: Y + 1,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X - 1},${Y - 1}`,
+    x: X - 1,
+    y: Y - 1,
+  },
+  {
+    alive: false,
+    dirty: false,
+    id: `${X + 1},${Y - 1}`,
+    x: X + 1,
+    y: Y - 1,
+  },
+];
 
 describe('CreatureNeighborsService', () => {
-  let service: CreatureNeighborsService;
+  let spectator: SpectatorService<CreatureNeighborsService>;
+  const createService = createServiceFactory({
+    service: CreatureNeighborsService,
+    mocks: [CreatureSelectorsService],
+  });
+
+  let mockCreatureSelectorsService: SpyObject<CreatureSelectorsService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CreatureNeighborsService);
+    spectator = createService();
+
+    mockCreatureSelectorsService = spectator.inject<CreatureSelectorsService>(
+      CreatureSelectorsService
+    );
+
+    mockCreatureSelectorsService.ById.and.callFake(
+      (id: TId) => creatureNeighbors.find((cell) => cell.id === id)!
+    );
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    const neighbors = spectator.service.GetNeighbors(baseCreature);
+
+    expect(mockCreatureSelectorsService.ById).toHaveBeenCalledTimes(
+      neighbors.length
+    );
+    expect(neighbors.length).toBe(8);
   });
 });
